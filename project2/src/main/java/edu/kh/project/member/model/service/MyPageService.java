7f5719318844fb5cd87cda2f8f4f0c33ad5faff8
luejenie <luejenie@gmail.com>
2022-10-27 11:43:56 +0900

    20221027

---
+++ project2/src/main/java/edu/kh/project/member/model/service/MyPageService.java
@@ -0,0 +1,7 @@
+package edu.kh.project.member.model.service;
+
+
+// 설계적 + 유지보수 + AOP
+public interface MyPageService {
+
+}
---
+++ project2/src/main/java/edu/kh/project/member/model/service/MyPageServiceImpl.java
@@ -0,0 +1,18 @@
+package edu.kh.project.member.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import edu.kh.project.member.model.dao.MyPageDAO;
+
+@Service  // bean 등록
+public class MyPageServiceImpl implements MyPageService{
+
+	@Autowired  // DI
+	private MyPageDAO dao;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;  // -> spring-security.xml에 작성되어 있음
+	
+}
